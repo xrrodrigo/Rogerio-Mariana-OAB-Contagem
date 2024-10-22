40c8b32b49a26548aa49739965d2cc537fe0701e
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2024-10-22 13:41:55 -0300

    Update square.js

--- purple/square.js
+++ purple/square.js
@@ -49,7 +49,7 @@ cropButton.addEventListener('click', function () {
             var imgInput = new Image();
             imgInput.src = croppedImage.src; // Imagem do input
             imgInput.onload = function() {
-                context.drawImage(imgInput, 0, 0, 500, 500);//canvas.width, canvas.height
+                context.drawImage(imgInput, 0, 0, 500, 500); //canvas.width, canvas.height
 
                  var img1 = new Image();
         img1.src = 'canvaimage.png'; // Caminho da primeira imagem (img1)
