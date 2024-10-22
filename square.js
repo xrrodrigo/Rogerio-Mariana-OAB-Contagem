f51b1e519e8b831096f146b1cff454421c79a90b
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2024-10-22 13:56:10 -0300

    Update square.js

--- square.js
+++ square.js
@@ -44,6 +44,12 @@ cropButton.addEventListener('click', function () {
         const canvas = document.getElementById('canvas');
         const context = canvas.getContext('2d');
 
+   var imgInput = new Image();
+            imgInput.src = croppedImage.src; // Imagem do input
+            imgInput.onload = function() {
+                context.drawImage(imgInput, 0, 0, canvas.width, canvas.height);
+
+        
         var img1 = new Image();
         img1.src = 'canvaimage.png'; // Caminho da primeira imagem (img1)
         img1.onload = function() {
@@ -51,11 +57,7 @@ cropButton.addEventListener('click', function () {
             canvas.height = img1.naturalHeight;
             context.drawImage(img1, 0, 0, canvas.width, canvas.height);
             
-            var imgInput = new Image();
-            imgInput.src = croppedImage.src; // Imagem do input
-            imgInput.onload = function() {
-                context.drawImage(imgInput, 0, 0, canvas.width, canvas.height);
-
+         
 
                 var img3 = new Image();
                 img3.src = 'overlay.png'; // Caminho da terceira imagem (img3)
